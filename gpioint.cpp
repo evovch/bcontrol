c1e4c139def261e6d90fb092f18ce718c887e4e0
korytov <korytov@mih.msk.rian>
2015-01-26 21:51:03 +0300

    - gpio interrupts

--- gpioint.cpp
+++ gpioint.cpp
@@ -63,7 +63,7 @@ void gpioInt::pollingLoop(unsigned int gpio)
         if (fdset[1].revents & POLLPRI) {
             len = read(fdset[1].fd, buf, MAX_BUF);
             printf("\npoll() GPIO %d interrupt occurred\n", gpio);
-            qDebug << "value: " << QString(buf);
+            qDebug() << "value: " << buf;
             if(buf[0] == '1')emit levelHigh();
             else emit levelLow();
         }
