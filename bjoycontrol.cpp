30ed612d7bdd93b1860f268d99abb91ee182b3f8
korytov <korytov@mih.msk.rian>
2015-01-26 19:13:48 +0300

    - restoring bJoy

--- bjoycontrol.cpp
+++ bjoycontrol.cpp
@@ -9,16 +9,16 @@ bJoyControl::bJoyControl(QObject *parent) :
 }
 
 void bJoyControl::_onSpeedChangedX(int val) {
-    socket->send("motor_pan", "set_speed", val * 5);
+    socket->send("motor_pan", "set_speed", QString::number(val * 5));
     qDebug() << "new Pan speed by bJoy: " << val;
 }
 
 void bJoyControl::_onSpeedChangedY(int val) {
-    socket->send("motor_tilt", "set_speed", val * 5);
+    socket->send("motor_tilt", "set_speed", QString::number(val * 5));
     qDebug() << "new Tilt speed by bJoy: " << val;
 }
 
 void bJoyControl::_onSpeedChangedZ(int val) {
-    socket->send("motor_ziim", "set_speed", val * 5);
+    socket->send("motor_ziim", "set_speed", QString::number(val * 5));
     qDebug() << "new Zoom speed by bJoy: " << val;
 }
